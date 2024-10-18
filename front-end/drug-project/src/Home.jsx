66a5b69c515dbcf68501709bfe4d8ba4eb0cf86e
merihan-amr <merihan.amr1@gmail.com>
2024-10-17 21:59:18 -0400

    trial 3

--- front-end/drug-project/src/Home.jsx
+++ front-end/drug-project/src/Home.jsx
@@ -2,19 +2,12 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div style={styles.pageContainer}>
+    <div style={{ textAlign: 'center', padding: '40px' }}>
       <h1>Welcome to ProHealth</h1>
       <p>Check drug interactions easily with our tool.</p>
     </div>
   );
 };
 
-const styles = {
-  pageContainer: {
-    textAlign: 'center',
-    color: '#fff',
-    marginTop: '100px',
-  },
-};
-
 export default Home;
+
