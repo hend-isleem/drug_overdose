b2c86ed7f192d94866c798c604d0cbea82588696
SallyAlsfadi <salisfadi@Salis-MacBook-Pro.local>
2024-10-25 14:54:27 -0400

    Automated-test for modification functionality

---
+++ src/components/MedicationInputForm.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import MedicationInputForm from "./MedicationInputForm";
+
+describe("MedicationInputForm", () => {
+  test("allows a user to add a drug to the list", () => {
+    render(<MedicationInputForm />);
+    const input = screen.getByPlaceholderText("Enter a drug name");
+    const addButton = screen.getByText("Add");
+
+    fireEvent.change(input, { target: { value: "Aspirin" } });
+    fireEvent.click(addButton);
+
+    expect(screen.getByText("Aspirin")).toBeInTheDocument();
+  });
+
+  test("allows a user to remove a drug from the list", () => {
+    render(<MedicationInputForm />);
+    const input = screen.getByPlaceholderText("Enter a drug name");
+    const addButton = screen.getByText("Add");
+
+    fireEvent.change(input, { target: { value: "Aspirin" } });
+    fireEvent.click(addButton);
+
+    const removeButton = screen.getByText("✖");
+    fireEvent.click(removeButton);
+
+    expect(screen.queryByText("Aspirin")).toBeNull();
+  });
+
+  test("allows a user to reset the drug list", () => {
+    render(<MedicationInputForm />);
+    const input = screen.getByPlaceholderText("Enter a drug name");
+    const addButton = screen.getByText("Add");
+    const resetButton = screen.getByText("Start over");
+
+    fireEvent.change(input, { target: { value: "Aspirin" } });
+    fireEvent.click(addButton);
+    fireEvent.click(resetButton);
+
+    expect(screen.queryByText("Aspirin")).toBeNull();
+  });
+});
