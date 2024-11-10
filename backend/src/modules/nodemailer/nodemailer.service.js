42f838f7a4a7f6c69291ab2ac9a6316cc347c223
PeterAbdalla <peter-adel-2007@hotmail.com>
2024-11-10 17:01:37 -0500

    added authentication module (layer)

---
+++ backend/src/modules/nodemailer/nodemailer.service.js
@@ -0,0 +1,104 @@
+const mjml2html = require('mjml');
+const nodemailer = require('nodemailer');
+const path = require('path');
+const pug = require('pug');
+const ApiError = require('../../utils/ApiError');
+const errorCode = require('../../codes/error.code');
+const config = require('../../config/config.config');
+const logger = require('../winston/winston.logger');
+
+const transport = nodemailer.createTransport(config.mail);
+
+transport
+  .verify()
+  .then(() => logger.info('Connected to email server'))
+  .catch((error) => logger.warn(`Unable to connect to email server, ${error.message}.`));
+
+/**
+ * Generate HTML from pug template
+ * @param {string} template - Template name in templates folder
+ * @param {object} data - Data to be used in email
+ * @returns {string} HTML
+ */
+const generateHTMLwPUG = (template, data) => {
+  const { html, errors } = mjml2html(pug.renderFile(path.join(__dirname, `../../templates/${template}.pug`), data));
+  if (errors && errors.length) throw new ApiError(errors[0].message);
+  return html;
+};
+
+/**
+ * Send an email
+ * @param {nodemailer.Transport} transporter - Transporter
+ * @param {object} mail - Mail
+ * @param {string} [mail.to] - User email
+ * @param {string} [mail.subject] - Email subject
+ * @param {string} [mail.html] - Email body in html
+ * @returns {Promise<object>} Object of acknowledgment
+ */
+const sendEmail = async (transporter, mail) => transporter.sendMail(mail);
+
+/**
+ * Send confirmation email
+ * @param {string} to - User email
+ * @param {string} name - User name
+ * @param {string} code - Verification code
+ * @param {function} t - Translation function
+ * @returns {Promise<object>} Object of acknowledgment
+ */
+const sendConfirmationEmail = async (to, name, code, t) =>
+  sendEmail(transport, {
+    from: config.SMTP.auth.user,
+    to,
+    subject: t(errorCode.CONFIRMATION_SUBJECT),
+    html: generateHTMLwPUG('confirmation', { name, code, t }),
+  });
+
+/**
+ * Send confirmation email
+ * @param {string} to - User email
+ * @param {string} code - Verification code
+ * @param {function} t - Translation function
+ * @returns {Promise<object>} Object of acknowledgment
+ */
+const sendResetPasswordEmail = async (to, code, t) =>
+  sendEmail(transport, {
+    from: config.mail.auth.user,
+    to,
+    subject: t(errorCode.RESET_SUBJECT),
+    html: generateHTMLwPUG('reset', { code, t }),
+  });
+
+/**
+ * Send registration email
+ * @param {string} to - User email
+ * @param {string} name - User name
+ * @param {string} password - password
+ * @param {function} t - Translation function
+ * @returns {Promise<object>} Object of acknowledgment
+ */
+const sendRegistrationEmail = async (to, name, password, t) =>
+  sendEmail(transport, {
+    from: config.mail.auth.user,
+    to,
+    subject: t(errorCode.REGISTRATION_SUBJECT),
+    html: generateHTMLwPUG('registration', { name, password, t }),
+  });
+
+/**
+ * Send an email
+ * @param {string} from - Contact Email
+ * @param {string} subject - Subject
+ * @param {string} text - Message
+ * @returns {Promise<object>} Object of acknowledgment
+ */
+const startConversation = async (subject, text) =>
+  sendEmail(transport, { from: config.mail.auth.user, to: config.mail.auth.user, subject, text });
+
+const nodemailerService = {
+  sendConfirmationEmail,
+  sendResetPasswordEmail,
+  sendRegistrationEmail,
+  startConversation,
+};
+
+module.exports = nodemailerService;
--- backend/src/modules/v1/auth/auth.route.js
+++ backend/src/modules/v1/auth/auth.route.js
@@ -5,54 +5,19 @@ const validate = require('../../../middlewares/validate.middleware');
 
 const authRoute = express.Router();
 
-/**
- * @swagger
- * tags:
- *   name: Auth
- *   description: API to authorize admin users
- */
-
-/**
- * @swagger
- * /v1/auth/admin-login:
- *   post:
- *     summary: Admin login to obtain an access token
- *     tags: [Auth]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *                 format: email
- *                 example: admin@sesa1.com
- *               password:
- *                 type: string
- *                 example: password1
- *     responses:
- *       200:
- *         description: Successfully authenticated
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 tokens:
- *                   type: object
- *                   properties:
- *                     access:
- *                       type: string
- *                       example: "Bearer eyJhbGciOi..."
- *                     refresh:
- *                       type: string
- *                       example: "Bearer eyJhbGciOi..."
- */
-
+authRoute.post('/register', validate(authValidation.register), authController.register);
+authRoute.post('/login', validate(authValidation.login), authController.login);
 authRoute.post('/admin-login', validate(authValidation.login), authController.adminLogin);
 authRoute.post('/logout', validate(authValidation.logout), authController.logout);
 authRoute.post('/refresh-tokens', validate(authValidation.logout), authController.refreshTokens);
+authRoute.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
+authRoute.post('/verify-code', validate(authValidation.verifyCode), authController.verifyCode);
+authRoute.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword);
+authRoute.post(
+  '/resend-confirmation-email',
+  validate(authValidation.forgotPassword),
+  authController.resendConfirmationEmail,
+);
+authRoute.post('/confirm-email', validate(authValidation.verifyCode), authController.confirmEmail);
 
 module.exports = authRoute;
